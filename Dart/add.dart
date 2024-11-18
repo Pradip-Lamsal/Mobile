d0db81b62cae395fdc9bb3734ffa7a68b547de52
Pradip Lamsal <pradiplamsal80@gmail.com>
2024-11-18 20:38:53 +0545

    five codes of dart

---
+++ Dart/add.dart
@@ -0,0 +1,11 @@
+// Function to add two numbers
+int addNumbers(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  int a = 10;
+  int b = 5;
+
+  print("Addition: ${addNumbers(a, b)}");
+}
---
+++ Dart/sub.dart
@@ -0,0 +1,11 @@
+// Function to subtract two numbers
+int subtractNumbers(int a, int b) {
+  return a - b;
+}
+
+void main() {
+  int a = 10;
+  int b = 5;
+
+  print("Subtraction: ${subtractNumbers(a, b)}");
+}
